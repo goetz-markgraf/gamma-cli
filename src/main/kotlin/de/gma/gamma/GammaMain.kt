bd88587b4a21ec8f37426b58da51707b8f755265
Goetz Markgraf <goeetz.markgraf@codecentric.de>
2022-05-18 22:15:52 +0200

    initial check-in

---
+++ src/main/kotlin/de/gma/gamma/GammaMain.kt
@@ -0,0 +1,90 @@
+package de.gma.gamma
+
+import de.gma.gamma.datatypes.expressions.Expression
+import de.gma.gamma.datatypes.functions.AbstractFunction
+import de.gma.gamma.datatypes.scope.ModuleScope
+import de.gma.gamma.datatypes.scope.Scope
+import de.gma.gamma.parser.EvaluationException
+import de.gma.gamma.parser.Parser
+
+fun main() {
+    var cmd: String
+
+    val buffer = StringBuffer()
+    var lastBuffer = ""
+
+    var scope = ModuleScope()
+
+    printHelp()
+
+    while (true) {
+
+        print("gamma> ")
+        val inp = readLine() ?: break
+
+        if (inp == "?" || inp == ".help" || inp == "help") {
+            printHelp()
+        } else if (inp == "." && buffer.isNotEmpty()) {
+            lastBuffer = buffer.toString()
+            execute(lastBuffer, scope)
+            buffer.delete(0, buffer.length)
+        } else if (inp.startsWith(".")) {
+            cmd = inp.drop(1)
+            when (cmd) {
+                "exit", "quit", "q", "bye" -> break
+
+                "clear", "c" -> buffer.delete(0, buffer.length)
+                "new" -> scope = ModuleScope()
+
+                "load", "l" -> {
+                    buffer.delete(0, buffer.length)
+                    buffer.append(lastBuffer)
+                    println(lastBuffer)
+                }
+            }
+        } else {
+            buffer.append(inp).append('\n')
+        }
+
+    }
+
+    println("Bye")
+}
+
+private fun printHelp() {
+    println("Gamma Interpreter REPL")
+    println("Commands")
+    println("   .      -> execute code and clear buffer")
+    println("   .clear -> clear the code buffer")
+    println("   .new   -> restart the REPL (delete all bindings)")
+    println("   .load  -> load code buffer from last executed code")
+    println("   .exit  -> exit the REPL")
+    println("   .quit  -> exit the REPL")
+    println("   .bye   -> exit the REPL")
+    println("   .help  -> display this help")
+}
+
+
+private fun execute(code: String, scope: Scope) {
+    println(code)
+
+    try {
+        val parser = Parser(code, "Script")
+        var expr = parser.nextExpression(-1)
+        while (expr != null) {
+            val result = expr.evaluate(scope)
+            if (result !is Expression && result !is AbstractFunction)
+                println("-> ${result.prettyPrint()}")
+
+            expr = parser.nextExpression(-1)
+        }
+
+        println()
+    } catch (l: EvaluationException) {
+        println()
+        println("**** Exception while evaluation code:\n${l::class.java} '${l.message}' in ${l.source}, line: ${l.line}, col: ${l.col}")
+    } catch (e: Exception) {
+        println()
+        println("**** Exception while evaluation code:\n${e::class.java} '${e.message}'")
+    }
+}
